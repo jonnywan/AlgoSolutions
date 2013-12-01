aa5298db01ac953ded3fceec9a7c81e94173812f
mitcc <mitcc@qq.com>
2013-12-01 19:22:19 +0800

    update

--- leetcode/ContainerWithMostWater.java
+++ leetcode/ContainerWithMostWater.java
@@ -7,15 +7,15 @@
  */
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
-        int max = 0;
+        int maxArea = 0;
         int low = 0, high = height.length - 1;
         while(low < high) {
-            max = Math.max(max, (high - low) * Math.min(height[low], height[high]));
+            maxArea = Math.max(maxArea, (high - low) * Math.min(height[low], height[high]));
             if(height[low] < height[high])
                 low++;
             else 
                 high--;
         }
-        return max;
+        return maxArea;
     }
 }
