a0d12f16fe229d0706e0d1acf290ef89b01160ad
mitcc <mitcc@qq.com>
2013-12-03 16:42:51 +0800

    update UniquePathII.java

--- leetcode/UniquePathsII.java
+++ leetcode/UniquePathsII.java
@@ -21,26 +21,23 @@
  */
 public class UniquePathsII {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int xLen = obstacleGrid.length;
-        int yLen = obstacleGrid[0].length;
+        int xLen = obstacleGrid.length, yLen = obstacleGrid[0].length;
         int[][] dp = new int[xLen][yLen];
-
+        dp[0][0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
         for(int i = 0; i < xLen; i++) {
             for(int j = 0; j < yLen; j++) {
-                if(obstacleGrid[i][j] == 1) 
+                if(obstacleGrid[i][j] == 1)
                     dp[i][j] = 0;
-                else {
-                    if(i == 0 && j == 0)
-                        dp[i][j] = 1;
-                    else if(i == 0 && j > 0)
-                        dp[i][j] = dp[i][j - 1];
-                    else if(j == 0 && i > 0)
-                        dp[i][j] = dp[i - 1][j];
-                    else 
-                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-                }
+                else if(i == 0 && j == 0)
+                    dp[i][j] = 1;
+                else if(i == 0 && j != 0) 
+                    dp[i][j] = dp[i][j - 1];
+                else if(i != 0 && j == 0)
+                    dp[i][j] = dp[i - 1][j];
+                else 
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        return dp[xLen - 1][yLen - 1];        
+        return dp[xLen - 1][yLen - 1];
     }
 }   
