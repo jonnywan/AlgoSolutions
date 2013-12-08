9b8043057cc95e139638f14396472221efa9da80
mitcc <mitcc@qq.com>
2013-12-08 11:06:00 +0800

    update

--- leetcode/MinimumDepthOfBinaryTree.java
+++ leetcode/MinimumDepthOfBinaryTree.java
@@ -17,9 +17,8 @@ public class MinimumDepthOfBinaryTree {
     public int minDepth(TreeNode root) {
         if(root == null) 
             return 0;
-        else if(root.left == null || root.right == null)
+        if(root.left == null || root.right == null)
             return 1 + Math.max(minDepth(root.left), minDepth(root.right));
-        else
-            return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
 }
