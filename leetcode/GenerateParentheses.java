07a2457c903af938af6fd6203aa68d7de892af09
mitcc <mitcc@qq.com>
2013-12-10 23:55:16 +0800

    update

--- leetcode/GenerateParentheses.java
+++ leetcode/GenerateParentheses.java
@@ -1,24 +1,43 @@
 /*
- * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * Given n pairs of parentheses, write a function to generate all combinations 
+ * of well-formed parentheses.
 
  * For example, given n = 3, a solution set is:
 
  * "((()))", "(()())", "(())()", "()(())", "()()()" 
  */
 public class GenerateParentheses {
+/************************** updated 2013/12/10 *******************************/
+
     public ArrayList<String> generateParenthesis(int n) {
-        ArrayList<String> result = new ArrayList<String>();
-        String subString = "";
-        generateParenthesisHelper(result, subString, n, n);
-        return result;
+        ArrayList<String> res = new ArrayList<String>();
+        helper(res, "", n, n);
+        return res;
+    }
+    
+    public void helper(ArrayList<String> array, String s, int left, int right) {
+        if(left == 0 && right == 0)
+            array.add(s);
+        if(left > 0)
+            helper(array, s + "(", left - 1, right);
+        if(left < right)
+            helper(array, s + ")", left, right - 1);
     }
 
-    public void generateParenthesisHelper(ArrayList<String> result, String subString, int left, int right) {
-        if(left == 0 && right == 0) 
-            result.add(subString);
-        if(left > 0)
-            generateParenthesisHelper(result, subString + "(", left - 1, right);
-        if(left < right) 
-            generateParenthesisHelper(result, subString + ")", left, right - 1);
+/*****************************************************************************/
+
+    public ArrayList<String> generateParenthesis(int n) {
+        ArrayList<String> res = new ArrayList<String>();
+        if(n == 0) 
+            res.add("");
+        else if(n == 1) 
+            res.add("()");
+        else {
+            for(int i = 0; i < n; i++) 
+                for(String inner : generateParenthesis(i)) 
+                    for(String outer : generateParenthesis(n - 1 - i))
+                        res.add("(" + inner + ")" + outer);
+        }
+        return res;
     }
 }
