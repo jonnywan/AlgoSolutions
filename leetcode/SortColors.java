d0c1ef55428416fa0ceee9c461f2e63bf7715d5d
mitcc <mitcc@qq.com>
2013-12-11 23:08:52 +0800

    update

--- leetcode/SortColors.java
+++ leetcode/SortColors.java
@@ -17,6 +17,19 @@
  * Could you come up with an one-pass algorithm using only constant space? 
  */
 public class SortColors {
+/**************************** updated 2013/12/11 *****************************/
+
+    public void sortColors(int[] A) {
+        int[] cnt = new int[3];
+        for(int i = 0; i < A.length; i++) 
+            cnt[A[i]]++;
+        for(int i = 0, index = 0; i < 3; i++)
+            for(int j = 0; j < cnt[i]; j++) 
+                A[index++] = i;
+    }
+
+/*****************************************************************************/
+
     public void sortColors(int[] A) {
         int redCount = 0, whiteCount = 0, blueCount = 0;
         for(int i = 0; i < A.length; i++) {
@@ -38,17 +51,18 @@ public class SortColors {
 /*****************************************************************************/
 
     public void sortColors(int[] A) {
-        int red = 0, blue = A.length - 1;
-        for(int i = 0; i <= blue; i++) {
-            if(A[i] == 0) {
-                int temp = A[i];
-                A[i] = A[red];
-                A[red++] = temp;
-            } else if(A[i] == 2) {
-                int temp = A[i];
-                A[i--] = A[blue];
-                A[blue--] = temp;
-            }
+        int left = 0, right = A.length - 1;
+        for(int i = 0; i <= right; i++) {
+            if(A[i] == 0)
+                swap(A, i, left++);
+            else if(A[i] == 2) 
+                swap(A, i--, right--);
         }
+    }
+
+    public void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
