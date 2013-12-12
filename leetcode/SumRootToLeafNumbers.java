755bd2706f57bc9e8f72aeab9c9f924759941073
mitcc <mitcc@qq.com>
2013-12-13 00:07:11 +0800

    update

--- leetcode/SumRootToLeafNumbers.java
+++ leetcode/SumRootToLeafNumbers.java
@@ -1,5 +1,6 @@
 /*
- * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path 
+ * could represent a number.
 
  * An example is the root-to-leaf path 1->2->3 which represents the number 123.
 
@@ -24,29 +25,34 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-package info.mitcc.leetcode;
-
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-	public TreeNode(int x) {
-		val = x;
-	}	
-}
 
 public class SumRootToLeafNumbers {
-	public int sumNumbers(TreeNode root) {
-		return sum(root, 0);
-	}
+/****************************** updated 2013/12/12 ***************************/
 
-	public int sum(TreeNode root, int prev) {
-		if(root == null)
-			return 0;
-		int result = 10 * prev + root.val;
-		if(root.left == null && root.right == null)
-			return result;
-		else
-			return sum(root.left, result) + sum(root.right, result);
-	}
+    public int sumNumbers(TreeNode root) {
+        if(root == null)
+            return 0;
+        if(root.left == null && root.right == null)
+            return root.val;
+        if(root.left != null)
+            root.left.val += 10 * root.val;
+        if(root.right != null)
+            root.right.val += 10 * root.val;
+        return sumNumbers(root.left) + sumNumbers(root.right);
+    }
+
+/*****************************************************************************/
+
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int sum) {
+        if(root == null)
+            return 0;
+        sum = 10 * sum + root.val;
+        if(root.left == null && root.right == null)
+            return sum;
+        return dfs(root.left, sum) + dfs(root.right, sum);
+    }
 }
