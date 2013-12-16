14b2379ab54daa7b09ab4be9f4f5d4d7c207d153
mitcc <mitcc@qq.com>
2013-12-16 23:18:18 +0800

    update

---
+++ project-euler/pe006.py
@@ -0,0 +1,25 @@
+__author__ = 'SUN'
+
+def sum_square_diff(start, end):
+    total = 0
+    for i in range(start, end):
+        for j in range(i + 1, end + 1):
+            total += i * j
+    total *= 2
+    return total
+
+def sum1(start, end):
+    total = 0
+    for i in range(start, end + 1):
+        total += i * i
+    return total
+
+def sum2(start, end):
+    return end * end * (end + 1) * (end + 1) // 4 - start * start * (start - 1) * (start - 1) // 4
+
+def sum_square_diff_1(start, end):
+    return sum2(start, end) - sum1(start, end)
+
+if __name__ == '__main__':
+    print(sum_square_diff_1(1, 100))
+    print(sum_square_diff(1, 100))
