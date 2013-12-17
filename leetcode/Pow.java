a4af54a9d3e2beea45270e0336049f0f3deced0e
mitcc <mitcc@qq.com>
2013-12-17 17:11:35 +0800

    update

--- leetcode/Pow.java
+++ leetcode/Pow.java
@@ -1,24 +1,17 @@
 /*
  * Implement pow(x, n).
  */
-package info.mitcc.leetcode;
 
 public class Pow {
-	public double pow(double x, int n) {
-//        double result = 1.00000;
-//        if(x == 0 && n <= 0)
-//        	throw new IllegalArgumentException();
-//        for(int i = 1; i <= Math.abs(n); i++) 
-//        	result *= x;
-//		return n >= 0 ? result : (1 / result);
-		
+    public double pow(double x, int n) {
         if(n == 0)
-        	return 1;
+            return 1;
         double half = pow(x, n / 2);
-        if(n % 2 == 0) 
-        	return half * half;
+        if(n % 2 == 0)
+            return half * half;
         else if(n > 0)
-        	return half * half * x;
-        return half * half / x;
+            return half * half * x;
+        else 
+            return half * half / x;
     }
 }
