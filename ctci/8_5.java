14b0d26877222da748916bbcc5c3582dfc059450
mitcc <mitcc@qq.com>
2013-12-18 23:20:51 +0800

    update

--- ctci/8_5.java
+++ ctci/8_5.java
@@ -1,5 +1,6 @@
 /*
- * Implement an algorithm to print all valid (e.g., properly opened and closed) combinations of n-pairs of parentheses.
+ * Implement an algorithm to print all valid (e.g., properly opened and closed) 
+ * combinations of n-pairs of parentheses.
  * EXAMPLE:
  * input: 3 (e.g., 3 pairs of parentheses)
  * output: ()()(), ()(()), (())(), ((()))
