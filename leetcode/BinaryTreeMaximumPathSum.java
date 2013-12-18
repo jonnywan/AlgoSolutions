7b0f7a4e3536e14dbe6345fb7d652a017fca9680
mitcc <mitcc@qq.com>
2013-12-18 16:02:43 +0800

    update

--- leetcode/BinaryTreeMaximumPathSum.java
+++ leetcode/BinaryTreeMaximumPathSum.java
@@ -20,28 +20,30 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-package info.mitcc.leetcode;
 
 import java.util.ArrayList;
 
 public class BinaryTreeMaximumPathSum {
-	int max;
-	public int maxPathSum(TreeNode root) {
-		max = Integer.MIN_VALUE;
-		maxSum(root);
-		return max;
-	}
-	public int maxSum(TreeNode root) {
-		if(root == null)
-			return 0;
-		int L = maxSum(root.left);
-		int R = maxSum(root.right);
-		int subMax = Math.max(Math.max(root.val, L + R + root.val), root.val + Math.max(L, R));
-		max = Math.max(max, subMax);
-		return Math.max(root.val, root.val + Math.max(L, R));
-	}
-	
-	/*public int maxPathSum(TreeNode root) {
+    int res = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maxSum(root);
+        return res;
+    }
+
+    public int maxSum(TreeNode root) {
+        if(root == null)
+            return 0;
+        int L = maxSum(root.left);
+        int R = maxSum(root.right);
+        int subMax = root.val + Math.max(Math.max(0, Math.max(L, R)), L + R);
+        if(res < subMax)
+            res = subMax;
+        return root.val + Math.max(0, Math.max(L, R));
+    }
+
+/*****************************************************************************/
+
+	/public int maxPathSum(TreeNode root) {
 		//pass the curmax in an array that contains only one value
 		ArrayList<Integer> curMax = new ArrayList<Integer>(1);
 		curMax.add(Integer.MIN_VALUE);
@@ -56,5 +58,5 @@ public class BinaryTreeMaximumPathSum {
 		int rightMax = Math.max(0, maxSubPath(root.right, curMax));
 		curMax.set(0, Math.max(curMax.get(0), root.val + leftMax + rightMax));
 		return Math.max(root.val + leftMax, root.val + rightMax);
-	}*/
+	}
 }
