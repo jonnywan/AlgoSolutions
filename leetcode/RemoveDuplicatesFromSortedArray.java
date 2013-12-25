fd2cab7ac18800209f19c9eb4c7a80e41f756598
mitcc <mitcc@qq.com>
2013-12-25 17:06:17 +0800

    update

--- leetcode/RemoveDuplicatesFromSortedArray.java
+++ leetcode/RemoveDuplicatesFromSortedArray.java
@@ -1,8 +1,9 @@
 /*
- * Given a sorted array, remove the duplicates in place such that each element appear only once 
- * and return the new length.
+ * Given a sorted array, remove the duplicates in place such that each element 
+ * appear only once and return the new length.
 
- * Do not allocate extra space for another array, you must do this in place with constant memory.
+ * Do not allocate extra space for another array, you must do this in place with 
+ * constant memory.
 
  * For example,
  * Given input array A = [1,1,2],
@@ -11,7 +12,7 @@
  * 
  */
 public class RemoveDuplicatesFromSortedArray {
-/*	public int removeDuplicates(int[] A) {
+	public int removeDuplicates(int[] A) {
 		int index = 0;
 		for(int i = 1; i < A.length; i++) {
 			if(A[i - 1] == A[i]) 
@@ -20,18 +21,18 @@ public class RemoveDuplicatesFromSortedArray {
 				A[i - index] = A[i];
 		}
 		return A.length - index;
-    }*/
+    }
+
+/*****************************************************************************/
 
     public int removeDuplicates(int[] A) {
         if(A.length == 0)
             return 0;
-        int index = 0;
+        int cnt = 1;
         for(int i = 1; i < A.length; i++) {
-            if(A[i] != A[index]) {
-                A[++index] = A[i];
-            }
+            if(A[i] != A[i - 1])
+                A[cnt++] = A[i];
         }
-        index++;
-        return index;        
+        return cnt;
     }
 }
