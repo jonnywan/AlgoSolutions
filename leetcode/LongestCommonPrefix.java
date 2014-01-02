a3898863efa348c1fc81c307830e9d9efe3c9873
mitcc <mitcc@qq.com>
2014-01-02 23:25:56 +0800

    2014.01.02, hey, how are you, my friend?

--- leetcode/LongestCommonPrefix.java
+++ leetcode/LongestCommonPrefix.java
@@ -1,20 +1,21 @@
 /*
- * Write a function to find the longest common prefix string amongst an array of strings.
+ * Write a function to find the longest common prefix string amongst an array 
+ * of strings.
  */
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         if(strs.length == 0)
             return "";
-        String result = strs[0];
+        int right_most = strs[0].length();
         for(int i = 1; i < strs.length; i++) {
-            result = result.substring(0, Math.min(result.length(), strs[i].length()));
-            for(int j = 0; j < result.length(); j++) {
-                if(result.charAt(j) != strs[i].charAt(j)) {
-                    result = result.substring(0, j);
+            right_most = Math.min(right_most, strs[i].length());
+            for(int j = 0; j < right_most; j++) {
+                if(strs[0].charAt(j) != strs[i].charAt(j)) {
+                    right_most = j;
                     break;
                 }
             }
         }
-        return result;
-    }
+        return strs[0].substring(0, right_most);
+    } 
 }
