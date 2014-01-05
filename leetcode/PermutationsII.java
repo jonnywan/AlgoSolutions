fff5fc9ed7bd09898b28e99d66b34b97d97236b5
mitcc <mitcc@qq.com>
2014-01-06 00:09:31 +0800

    2014.01.06 add DFS method

--- leetcode/PermutationsII.java
+++ leetcode/PermutationsII.java
@@ -24,4 +24,35 @@ public class PermutationsII {
 		}
 		return res;
     }
+
+/***************************** updated 2014/01/06 ****************************/
+
+    public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(num);
+        boolean[] isVisited = new boolean[num.length];
+        dfs(res, list, num, isVisited);
+        return res; 
+    }
+
+    public void dfs(ArrayList<ArrayList<Integer>> res , ArrayList<Integer> list, 
+            int[] num, boolean[] isVisited) {
+        if(list.size() == num.length) {
+            res.add(new ArrayList<Integer>(list));
+            return ;
+        }
+        for(int i = 0; i < num.length; i++) {
+            if(isVisited[i] == false) {
+                isVisited[i] = true;
+                list.add(num[i]);
+                dfs(res, list, num, isVisited);
+                isVisited[i] = false;
+                list.remove(list.size() - 1);
+                while(i < num.length - 1 && num[i] == num[i + 1]) {
+                    i++;
+                }
+            }   
+        }
+    }
 }
