fce4f30e16d0ced8384b9348ed1a0183a96dfaec
mitcc <mitcc@qq.com>
2014-01-05 18:38:26 +0800

    2014.01.05 add a DFS method

--- leetcode/Permutations.java
+++ leetcode/Permutations.java
@@ -8,26 +8,51 @@
 import java.util.*;
 
 public class Permutations {
-/*************************** updated 2013/11/29 ***********************************/
+/*************************** updated 2013/11/29 ******************************/
 
     public ArrayList<ArrayList<Integer>> permute(int[] num) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        if(num.length > 0) {
-            ArrayList<Integer> init = new ArrayList<Integer>();
-            init.add(num[0]);
-            res.add(init);
-            for(int i = 1; i < num.length; i++) {
-                ArrayList<ArrayList<Integer>> newRes = new ArrayList<ArrayList<Integer>>();
-                for(int j = 0; j < res.size(); j++) {
-                    for(int k = 0; k <= res.get(j).size(); k++) {
-                        ArrayList<Integer> list = new ArrayList<Integer>(res.get(j));
-                        list.add(k, num[i]);
-                        newRes.add(list);
-                    }
+        if(num.length == 0)
+            return res;
+        res.add(new ArrayList<Integer>());
+        for(int i = 0; i < num.length; i++) {
+            ArrayList<ArrayList<Integer>> newRes = new ArrayList<ArrayList<Integer>>();
+            for(int j = 0; j < res.size(); j++) {
+                for(int k = 0; k <= res.get(j).size(); k++) {
+                    ArrayList<Integer> list = new ArrayList<Integer>(res.get(j));
+                    list.add(k, num[i]);
+                    newRes.add(list);
                 }
-                res = newRes;
             }
+            res = newRes;
         }
         return res;
+    }
+
+/**************************** updated 2014/01/05 *****************************/
+
+    public ArrayList<ArrayList<Integer>> permute(int[] num) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        boolean[] isVisited = new boolean[num.length];
+        dfs(res, list,isVisited, num);
+        return res;
+    }
+
+    public void dfs(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> list,
+            boolean[] isVisited, int[] num) {
+        if(list.size() == num.length) {
+            res.add(new ArrayList<Integer>(list));
+            return ;
+        }
+        for(int i = 0; i < num.length; i++) {
+            if(isVisited[i] == false) {
+                isVisited[i] = true;
+                list.add(num[i]);
+                dfs(res, list, isVisited, num);
+                isVisited[i] = false;
+                list.remove(list.size() - 1);
+            }
+        }
     }
 }
