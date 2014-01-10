73b69db251bb0867f30b7764f450695398fd210a
mitcc <mitcc@qq.com>
2014-01-10 09:39:27 +0800

    2014.01.10 updated Library

--- basic/Library.java
+++ basic/Library.java
@@ -12,20 +12,6 @@ public class Library {
         }
         return a;
     }
-
-    public boolean isPrime(int n) {
-        if(n < 0)
-            throw new IllegalArgumentException("negative number");
-        if(n == 0 || n == 1)
-            return false;
-        if(n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
-            return n == 2 || n == 3 || n == 5 || n == 7;
-        for(int i = 7; i * i <= n; i += 2) {
-            if(n % i == 0)
-                return false;
-        }
-        return true;
-    }
 }
 
 
