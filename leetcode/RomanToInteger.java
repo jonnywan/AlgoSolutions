051965dc05ae53c7141de5b528aff2afeb6b8b6c
mitcc <mitcc@qq.com>
2014-01-18 23:10:26 +0800

    2014.01.18 fast!

--- leetcode/RomanToInteger.java
+++ leetcode/RomanToInteger.java
@@ -9,10 +9,17 @@
  * C = 100;
  * D = 500;
  * M = 1000;
+ *
+ * the numeral I can be placed before V and X to make 4 units (IV) and 9 units 
+ * (IX respectively)
+ * X can be placed before L and C to make 40 (XL) and 90 (XC respectively)
+ * C can be placed before D and M to make 400 (CD) and 900 (CM) according to 
+ * the same pattern
+ * 
  */
 public class RomanToInteger {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
@@ -20,14 +27,10 @@ public class RomanToInteger {
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
-        int result = 0;
-        for(int i = 0; i < s.length() - 1; i++) {
-            if(map.get(s.charAt(i)) < map.get(s.charAt(i + 1)))
-                result -= map.get(s.charAt(i));
-            else
-                result += map.get(s.charAt(i));
-        }
-        result += map.get(s.charAt(s.length() - 1));
-        return result;
+        int res = map.get(s.charAt(s.length() - 1));
+        for(int i = s.length() - 2; i >= 0; i--) 
+            res += (map.get(s.charAt(i)) < map.get(s.charAt(i + 1)) ? -1 : 1) * 
+                map.get(s.charAt(i));
+        return res;
     }
 }
