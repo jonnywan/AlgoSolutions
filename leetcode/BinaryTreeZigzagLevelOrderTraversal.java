8d968912b3b681e6316194ccf8b6aecdf70a9b2a
mitcc <mitcc@qq.com>
2014-01-18 19:54:02 +0800

    2014.01.18 sleep!

--- leetcode/BinaryTreeZigzagLevelOrderTraversal.java
+++ leetcode/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,6 +1,8 @@
 /*
- * Given a binary tree, return the zigzag level order traversal of its nodes' values. 
- * (ie, from left to right, then right to left for the next level and alternate between).
+ * Given a binary tree, return the zigzag level order traversal of its nodes' 
+ * values. 
+ * (ie, from left to right, then right to left for the next level and alternate 
+ * between).
 
  * For example:
  * Given binary tree {3,9,20,#,#,15,7},
@@ -20,31 +22,31 @@
  */
 public class BinaryTreeZigzagLevelOrderTraversal {
     public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
         if(root == null) 
-            return result;
+            return res;
         ArrayList<Integer> saveNodeVal = new ArrayList<Integer>();
         LinkedList<TreeNode> curLevel = new LinkedList<TreeNode>();
         LinkedList<TreeNode> nextLevel = new LinkedList<TreeNode>();
-        boolean isL2R = true;
         curLevel.add(root);
+        boolean isL2R = true;
         while(!curLevel.isEmpty()) {
             TreeNode cur = curLevel.remove();
+            saveNodeVal.add(cur.val);
             if(cur.left != null)
                 nextLevel.add(cur.left);
             if(cur.right != null) 
                 nextLevel.add(cur.right);
-            saveNodeVal.add(cur.val);
             if(curLevel.isEmpty()) {
                 if(!isL2R)
                     Collections.reverse(saveNodeVal);
-                result.add(saveNodeVal);
                 isL2R = !isL2R;
+                res.add(saveNodeVal);
                 curLevel = nextLevel;
                 nextLevel = new LinkedList<TreeNode>();
                 saveNodeVal = new ArrayList<Integer>();
             }
         }   
-        return result;
+        return res;
     }
 }
