4504fd7a3d65e318ad21ef8622337197c23377fd
mitcc <mitcc@qq.com>
2014-01-20 01:22:46 +0800

    2014.01.20 Best wishes to you!

--- leetcode/NQueens.java
+++ leetcode/NQueens.java
@@ -40,12 +40,8 @@ public class NQueens {
             String[] s = new String[n];
             for(int i = 0; i < n; i++) {
                 s[i] = "";
-                for(int j = 0; j < n; j++) {
-                    if(j != row[i])
-                        s[i] += ".";
-                    else
-                        s[i] += "Q";
-                }
+                for(int j = 0; j < n; j++) 
+                    s[i] += (j != row[i]) ? "." : "Q";
             }
             res.add(s);
         }
@@ -64,5 +60,42 @@ public class NQueens {
                 col[i] = 0;
             }
         }
+    }
+
+/****************************** updated 2014.01.20 ***************************/
+
+    public ArrayList<String[]> solveNQueens(int n) {
+        ArrayList<String[]> res = new ArrayList<String[]>();
+        int[] board = new int[n];
+        placeQueenOnBoard(res, 0, board);
+        return res;
+    }
+
+    public void placeQueenOnBoard(ArrayList<String[]> res, int row, int[] board) {
+        int n = board.length;
+        if(row == n) {
+            String[] s = new String[n];
+            for(int i = 0; i < n; i++) {
+                s[i] = "";
+                for(int j = 0; j < n; j++) 
+                    s[i] += (j != board[i]) ? "." : "Q";
+            }
+            res.add(s);
+        } else {
+            for(int col = 0; col < n; col++) {
+                if(isSafePlace(row, col, board)) {
+                    board[row] = col;
+                    placeQueenOnBoard(res, row + 1, board);
+                    board[row] = -1;
+                }
+            }
+        }
+    }
+
+    public boolean isSafePlace(int row, int col, int[] board) {
+        for(int i = 0; i < row; i++)
+            if(board[i] == col || Math.abs(board[i] - col) == Math.abs(i - row))
+                return false;
+        return true;
     }
 }
