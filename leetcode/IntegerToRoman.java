b033cf388cd23b782c981c7b31f1df1aef8c2f93
mitcc <mitcc@qq.com>
2014-01-19 11:01:22 +0800

    2014.01.19 hello~

--- leetcode/IntegerToRoman.java
+++ leetcode/IntegerToRoman.java
@@ -24,4 +24,32 @@ public class IntegerToRoman {
         result += ones[num % 10];
         return result;
     }
+
+/****************************** updated 2014.01.19 ***************************/
+
+    public String intToRoman(int num) {
+        String[] roman = {"I", "X", "C", "M", "V", "L", "D"};
+        int i = 0;
+        String res = "";
+        while(num > 0) {
+            int low = num % 10;
+            if(low >= 1 && low <= 3)
+                for(int j = 0; j < low; j++)
+                    res = roman[i] + res;
+            if(low == 4)
+                res = roman[i] + roman[i + 4] + res;
+            if(low == 5)
+                res = roman[i + 4] + res;
+            if(low >= 6 && low <= 8) {
+                for(int j = 0; j < low - 5; j++)
+                    res = roman[i] + res;
+                res = roman[i + 4] + res;
+            }
+            if(low == 9)
+                res = roman[i] + roman[i + 1] + res;
+            num /= 10;
+            i++;
+        }
+        return res;
+    }
 }
