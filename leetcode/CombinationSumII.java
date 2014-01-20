707e3730c50b1c63b8f42fbb66591984e6bf3e84
mitcc <mitcc@qq.com>
2014-01-20 16:39:15 +0800

    2014.01.20 Dinner

--- leetcode/CombinationSumII.java
+++ leetcode/CombinationSumII.java
@@ -1,5 +1,6 @@
 /*
- * Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in 
+ * Given a collection of candidate numbers (C) and a target number (T), find all 
+ * unique combinations in 
  * C where the candidate numbers sums to T.
 
  * Each number in C may only be used once in the combination.
@@ -7,7 +8,8 @@
  * Note:
 
  * All numbers (including target) will be positive integers.
- * Elements in a combination (a1, a2, � , ak) must be in non-descending order. (ie, a1 ? a2 ? � ? ak).
+ * Elements in a combination (a1, a2, � , ak) must be in non-descending order. 
+ * (ie, a1 ? a2 ? � ? ak).
  * The solution set must not contain duplicate combinations.
  * For example, given candidate set 10,1,2,7,6,1,5 and target 8, 
  * A solution set is: 
@@ -18,24 +20,24 @@
  */
 public class CombinationSumII {
     public ArrayList<ArrayList<Integer>> combinationSum2(int[] num, int target) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> subList = new ArrayList<Integer>();
         Arrays.sort(num);
-        combinationSum2Helper(result, subList, num, target, 0, 0);
-        return result;
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        dfs(res, list, num, 0, 0, target);
+        return res;
     }
 
-    public void combinationSum2Helper(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> subList, 
-            int[] num, int target, int sum, int preIndex) {
-        if(sum == target) 
-            result.add(new ArrayList<Integer>(subList));
+    public void dfs(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> list,
+            int[] num, int pos, int sum, int target) {
+        if(sum == target)
+            res.add(new ArrayList<Integer>(list));
         if(sum < target) {
-            for(int i = preIndex; i < num.length; i++) {
-                if(i > preIndex && num[i] == num[i - 1])
+            for(int i = pos; i < num.length; i++) {
+                if(i > pos && num[i] == num[i - 1])
                     continue;
-                subList.add(num[i]);
-                combinationSum2Helper(result, subList, num, target, sum + num[i], i + 1);
-                subList.remove(subList.size() - 1);
+                list.add(num[i]);
+                dfs(res, list, num, i + 1, sum + num[i], target);
+                list.remove(list.size() - 1);
             }
         }
     }
