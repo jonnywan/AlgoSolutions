380c903d9e485545a99aec81185cebb504a8c3e9
mitcc <mitcc@qq.com>
2014-01-22 00:39:31 +0800

    2014.01.22 updated

--- leetcode/UniqueBinarySearchTreesII.java
+++ leetcode/UniqueBinarySearchTreesII.java
@@ -1,5 +1,6 @@
 /*
- * Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+ * Given n, generate all structurally unique BST's (binary search trees) that 
+ * store values 1...n.
 
  * For example,
  * Given n = 3, your program should return all 5 unique BST's shown below.
