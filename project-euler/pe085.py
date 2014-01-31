fceaddf423dca8dee80c0d6aa15ec791bf3e7021
mitcc <mitcc@qq.com>
2014-01-31 10:34:13 +0800

    2014.01.31

--- project-euler/pe085.py
+++ project-euler/pe085.py
@@ -1,3 +1,13 @@
+'''
+Counting rectangles
+Problem 85
+
+By counting carefully it can be seen that a rectangular grid measuring 3 by 2
+contains eighteen rectangles:
+	http://projecteuler.net/problem=85
+Although there exists no rectangular grid that contains exactly two million
+rectangles, find the area of the grid with the nearest solution.
+'''
 __author__ = 'SUN'
 
 if __name__ == '__main__':
