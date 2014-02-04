87dc025fcba7968b80407e941b7eff28b168befa
mitcc <mitcc@qq.com>
2014-02-05 02:45:10 +0800

    2014.02.05

--- project-euler/pe122.py
+++ project-euler/pe122.py
@@ -1,8 +1,10 @@
+# not completed!
+
 __author__ = 'SUN'
 
 if __name__ == '__main__':
     cnt = 0
-    for i in range(101):
+    for i in range(10000):
         if i % 2 != 0 and i % 3 != 0:
             cnt += 1
             print(i)
