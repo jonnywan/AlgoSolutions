f420abbfe97275b65fad52069741ae2a126d4149
mitcc <mitcc@qq.com>
2014-02-08 00:42:53 +0800

    2014.02.08

--- project-euler/pe047.py
+++ project-euler/pe047.py
@@ -1,6 +1,21 @@
+'''
+Distinct primes factors
+Problem 47
+
+The first two consecutive numbers to have two distinct prime factors are:
+                     14 = 2 × 7
+                     15 = 3 × 5
+The first three consecutive numbers to have three distinct prime factors are:
+
+                      644 = 2² × 7 × 23
+                      645 = 3 × 5 × 43
+                      646 = 2 × 17 × 19.
+Find the first four consecutive integers to have four distinct prime factors.
+What is the first of these numbers?
+'''
 __author__ = 'SUN'
 
-def distinct_prime_number(n):
+def distinct_prime_factor(n):
     cnt = 0
     start = 2
     end = int(n ** 0.5)
@@ -17,6 +32,6 @@ def distinct_prime_number(n):
 
 if __name__ == '__main__':
     i = 2
-    while not distinct_prime_number(i) == distinct_prime_number(i + 1) == distinct_prime_number(i + 2) == distinct_prime_number(i + 3) == 4:
+    while not distinct_prime_factor(i) == distinct_prime_factor(i + 1) == distinct_prime_factor(i + 2) == distinct_prime_factor(i + 3) == 4:
         i += 1
     print(i)
