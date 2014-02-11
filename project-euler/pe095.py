5f249608c09ab6a5793e63acad079175722b660e
mitcc <mitcc@qq.com>
2014-02-11 23:47:44 +0800

    2014.02.11

--- project-euler/pe095.py
+++ project-euler/pe095.py
@@ -23,4 +23,29 @@ one million.
 
 __author__ = 'SUN'
 
+def divisor_sum(n):
+    s = 1
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            s += i + n // i
+    if int(n ** 0.5) ** 2 == n:
+        s -= int(n ** 0.5)
+    return s
+
+def chain(n):
+    chains = []
+    while not chains.__contains__(n):
+        chains.append(n)
+        n = divisor_sum(n)
+        if n > 1000000:
+            break
+    return chains
+
 if __name__ == '__main__':
+    array = []
+    for i in range(1000000):
+        print(i)
+        ch = chain(i)
+        if len(array) < len(ch):
+            array = ch
+            print('array[0] = ', array[0])
