3f3018245a4a6b5c4868b171b0fd33dad8f94d3b
mitcc <mitcc@qq.com>
2014-02-18 15:58:49 +0800

    2014.02.18

--- project-euler/pe117.py
+++ project-euler/pe117.py
@@ -17,16 +17,14 @@ __author__ = 'SUN'
 def f(n):
     ways = [0] * (n + 1)
     ways[0] = 1
-    for i in range(n + 1):
-        #if i >= 1:
-        #    ways[i] += ways[i - 1]
-        #if i >= 2:
-        #    ways[i] += ways[i - 2]
-        #if i >= 3:
-        #    ways[i] += ways[i - 3]
-        #if i >= 4:
-        #    ways[i] += ways[i - 4]
-        ways[i] += ways[i - 1] + ways[i - 2] + ways[i - 3] + ways[i - 4]
+    for i in range(1, n + 1):
+        ways[i] += ways[i - 1]
+        if i >= 2:
+            ways[i] += ways[i - 2]
+        if i >= 3:
+            ways[i] += ways[i - 3]
+        if i >= 4:
+            ways[i] += ways[i - 4]
     return ways[n]
 
 if __name__ == '__main__':
