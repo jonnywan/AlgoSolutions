ae25e594a8bf5a46418dd87d2498199f62b546dc
mitcc <mitcc@qq.com>
2014-02-24 21:39:53 +0800

    2014.02.24

--- leetcode/AddBinary.java
+++ leetcode/AddBinary.java
@@ -8,20 +8,17 @@
  */
 public class AddBinary {
     public String addBinary(String a, String b) {
-        if(a.length() == 0)
-            return b;
-        if(b.length() == 0)
-            return a;
-        String result = "";
+        int len = Math.max(a.length(), b.length());
+        String res = "";
         int carry = 0;
-        for(int i = 0; i < Math.max(a.length(), b.length()); i++) {
-            int x = (i > a.length() - 1) ? 0 : a.charAt(a.length() - 1 - i) - '0';
-            int y = (i > b.length() - 1) ? 0 : b.charAt(b.length() - 1 - i) - '0';
-            result = (x + y + carry) % 2 + result;
-            carry = (x + y + carry) / 2;
+        for (int i = 0; i < len; i++) {
+            int ca = i < a.length() ? a.charAt(a.length() - i - 1) - '0' : 0;
+            int cb = i < b.length() ? b.charAt(b.length() - i - 1) - '0' : 0;
+            res = (ca + cb + carry) % 2 + res;
+            carry = (ca + cb + carry) / 2;
         }
-        if(carry > 0)
-            result = carry % 2 + result;
-        return result;
+        if (carry > 0)
+            res = carry + res;
+        return res;
     }
 }
