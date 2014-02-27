84911ba370c68510fd4467f03ba6eb057dc210af
mitcc <mitcc@qq.com>
2014-02-28 01:31:01 +0800

    2014.02.28

--- project-euler/pe087.py
+++ project-euler/pe087.py
@@ -36,7 +36,22 @@ if __name__ == '__main__':
         i += 1
     i -= 1
 
-    print(prime(100))
-    print(prime(int(N ** 0.25) + 1)[-1])
-    print(7069 ** 2)
-    print(83 ** 4)
+    m2 = m3 = m4 = 0
+    a = prime(10000)
+    for i in range(len(a)):
+        if a[i - 1] ** 2 < N <= a[i] ** 2:
+            m2 = a[i - 1]
+            break
+        if a[i - 1] ** 2 < N <= a[i] ** 3:
+            m3 = a[i - 1]
+        if a[i - 1] ** 4 < N <= a[i] ** 4:
+            m4 = a[i - 1]
+
+    res = []
+    p = prime(m2 + 1)
+    for p4 in range(p.index(m4) + 1):
+        for p3 in range(p.index(m3) + 1):
+            for p2 in range(len(p)):
+                if p[p2] ** 2 + p[p3] ** 3 + p[p4] ** 4 < N:
+                    res.append(p[p2] ** 2 + p[p3] ** 3 + p[p4] ** 4)
+    print('total number is ', len(set(res)))
