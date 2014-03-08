77fcd7624eba0201a51860971bc1eb7f6be78dae
mitcc <mitcc@qq.com>
2014-03-09 00:03:32 +0800

    2014.03.09

---
+++ project-euler/pe075.py
@@ -0,0 +1,47 @@
+'''
+Singular integer right triangles
+Problem 75
+It turns out that 12 cm is the smallest length of wire that can be bent to form
+an integer sided right angle triangle in exactly one way, but there are many
+more examples.
+
+12 cm: (3,4,5)
+24 cm: (6,8,10)
+30 cm: (5,12,13)
+36 cm: (9,12,15)
+40 cm: (8,15,17)
+48 cm: (12,16,20)
+
+In contrast, some lengths of wire, like 20 cm, cannot be bent to form an integer
+sided right angle triangle, and other lengths allow more than one solution to be
+found; for example, using 120 cm it is possible to form exactly three different
+integer sided right angle triangles.
+
+120 cm: (30,40,50), (20,48,52), (24,45,51)
+
+Given that L is the length of the wire, for how many values of L ≤ 1,500,000 can
+exactly one integer sided right angle triangle be formed?
+'''
+
+__date__ = '14-3-8'
+__author__ = 'SUN'
+
+import time
+
+def gcd(m, n):
+    while n != 0:
+        m, n = n, m % n
+    return m
+
+if __name__ == '__main__':
+    start = time.clock()
+    N = 1500000
+    triangle = [0] * (N + 1)
+    for i in range(1, int(N ** 0.5), 2):
+        for j in range(2, int(N ** 0.5) - i, 2):
+            if gcd(i, j) == 1:
+                s = abs(i ** 2 - j ** 2) + 2 * i * j + i ** 2 + j ** 2
+                for k in range(s, N + 1, s):
+                    triangle[k] += 1
+    print(triangle.count(1))
+    print('Runtime is ', time.clock() - start)
