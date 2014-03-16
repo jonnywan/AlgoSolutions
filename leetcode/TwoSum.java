7edb2e3f5c8c97791d533073c8fd035db818e46c
mitcc <mitcc@qq.com>
2014-03-16 18:41:49 +0800

    2014.03.16

--- leetcode/TwoSum.java
+++ leetcode/TwoSum.java
@@ -1,31 +1,31 @@
-public class TowSum {
-    public int[] twoSum(int[] numbers, int target) {
-        /*int[] result = new int[2];
-        for(int i = 0; i < numbers.length - 1; i++) {//from 0 to length - 2;
-            for(int j = i + 1; j < numbers.length; j++) {
-                if(numbers[i] + numbers[j] == target) {
-                    result[0] = i + 1; 
-                    result[1] = j + 1;
-                    return result;
-                }
-            }
-        }
-        return result;*/
+/*
+ * Given an array of integers, find two numbers such that they add up to a 
+ * specific target number.
 
-        int[] result = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i = 0; i < numbers.length; i++) {//from 0 to length - 1;   
-            if(map.containsKey(numbers[i])) {
-                int index = map.get(numbers[i]);
-                if(index < i) {
-                    result[0] = index + 1;
-                    result[1] = i + 1;
-                    break;
-                }
+ * The function twoSum should return indices of the two numbers such that they 
+ * add up to the target, where index1 must be less than index2. Please note 
+ * that your re * turned answers (both index1 and index2) are not zero-based.
+
+ * You may assume that each input would have exactly one solution.
+
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2
+ * 
+ */
+public class TowSum {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (!map.containsKey(numbers[i])) 
+                map.put(target - numbers[i], i + 1);
+            else {
+                res[0] = map.get(numbers[i]);
+                res[1] = i + 1;
+                break;
             }
-            if(!map.containsKey(numbers[i]))
-                map.put(target - numbers[i], i);
         }
-        return result;
+        return res;
     }
 }
