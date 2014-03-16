8cbc7d8c99efdb4ba93c5ccba3460e2add2edc4c
mitcc <mitcc@qq.com>
2014-03-16 17:21:34 +0800

    2014.03.16

--- leetcode/AddTwoNumbers.java
+++ leetcode/AddTwoNumbers.java
@@ -1,6 +1,8 @@
 /*
- * You are given two linked lists representing two non-negative numbers. The digits are stored in reverse 
- * order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * You are given two linked lists representing two non-negative numbers. The 
+ * digits are stored in reverse 
+ * order and each of their nodes contain a single digit. Add the two numbers 
+ * and return it as a linked list.
 
  * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
  * Output: 7 -> 0 -> 8 
@@ -22,39 +24,43 @@ public class AddTwoNumbers {
     }
 
     public ListNode addTwoNumbersHelper(ListNode l1, ListNode l2, int carry) {
-        if(l1 == null && l2 == null && carry == 0)
-            return null;
-        if(l1 == null && l2 == null && carry != 0)
-            return new ListNode(carry);
+        if (l1 == null && l2 == null) 
+            return carry == 0 ? null : new ListNode(carry);
         int i = 0, j = 0;
         if(l1 != null)
             i = l1.val;
         if(l2 != null)
             j = l2.val;
         ListNode head = new ListNode((i + j + carry) % 10);
-        head.next = addTwoNumbersHelper((l1 == null ? null : l1.next), (l2 == null ? null : l2.next), (i + j + carry) / 10);
+        head.next = addTwoNumbersHelper(l1 == null ? null : l1.next, 
+                l2 == null ? null : l2.next, (i + j + carry) / 10);
         return head;
     }
 
-/*  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+/**************************** updated 2014.03.16 *****************************/
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
-        ListNode result = new ListNode(0);
-        ListNode cur1 = l1, cur2 = l2, head = result;
-        while(cur1 != null || cur2 != null) {
-            if(cur1 != null) {
-                carry += cur1.val;
-                cur1 = cur1.next;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null || l2 != null) {
+            int a = 0;
+            int b = 0;
+            if (l1 != null) {
+                a = l1.val;
+                l1 = l1.next;
             }
-            if(cur2 != null) {
-                carry += cur2.val;
-                cur2 = cur2.next;
+            if (l2 != null) {
+                b = l2.val;
+                l2 = l2.next;
             }
-            head.next = new ListNode(carry % 10);
-            head = head.next;
-            carry /= 10;
+            cur.next = new ListNode((a + b + carry) % 10);
+            carry = (a + b + carry) / 10;
+            cur = cur.next;
         }
-        if(carry == 1)
-            head.next = new ListNode(carry);
-        return result.next;
-    }*/
+        if (carry != 0)
+            cur.next = new ListNode(carry);
+        return dummy.next;
+    }
 }
