6825498b5c18e7257c7ac66de63357461905e4a3
mitcc <mitcc@qq.com>
2014-03-17 16:40:38 +0800

    2014.03.17

--- project-euler/pe034.py
+++ project-euler/pe034.py
@@ -10,25 +10,23 @@ Note: as 1! = 1 and 2! = 2 are not sums thetemp are not included.
 __date__ = '14-3-17'
 __author__ = 'SUN'
 
-def factorial(k):
-    f = 1
-    for i in range(10, k + 1):
-        f *= i
-    return f
+import time
 
 if __name__ == '__main__':
+    start = time.clock()
+    factorial = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
     n = 1
-    f9 = factorial(9)
-    while n * f9 >= 10 ** n - 1:
+    while n * factorial[9] >= 10 ** n - 1:
         n += 1
     res = 0
-    for x in range(10, 10000000):
+    for x in range(10, 10 ** n):
         s = 0
         temp = x
         while temp > 0:
-            s += factorial(temp % 10)
+            s += factorial[temp % 10]
             temp //= 10
         if s == x:
             print(x)
             res += s
     print(res)
+    print('Runtime is ', time.clock() - start)
