0496c770f2672bb9cc914d149f9df1fc3b3a3c6d
mitcc <mitcc@qq.com>
2014-03-22 00:30:38 +0800

    2014.03.22

--- project-euler/pe039.py
+++ project-euler/pe039.py
@@ -14,19 +14,36 @@ import time
 __date__ = '14-3-21'
 __author__ = 'SUN'
 
-if __name__ == '__main__':
-    start = time.clock()
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def project_euler_1(limit):
     res = 0
     cnt = 0
     for k in range(12, 1001):
         n = 0
-        for i in range(1, int(k * 0.3)):
+        for i in range(1, int(k * (1 - math.sqrt(2) / 2))):
             for j in range(int(k * (1 - math.sqrt(2) / 2)), k // 2):
                 if i ** 2 + j ** 2 == (k - i - j) ** 2:
                     n += 1
-                    print('k = ', k, ', a = ', i, ', b = ', j, ', c = ', k - i - j)
         if cnt < n:
             res = k
             cnt = n
-    print('res = ', res, ', cnt = ', cnt)
+    print(res)
+
+def project_euler_2(limit):
+    t = [0] * (limit + 1)
+    for i in range(2, int((limit // 2) ** 0.5) + 1):
+        for j in range(1, i):
+            if (i + j) % 2 == 1 and gcd(i, j) == 1:
+                s = 2 * i * (i + j)
+                for x in range(s, limit + 1, s):
+                    t[x] += 1
+    print(t.index(max(t)))
+
+if __name__ == '__main__':
+    start = time.clock()
+    project_euler_1(1000)
     print('Runtime is ', time.clock() - start)
--- project-euler/pe271.py
+++ project-euler/pe271.py
@@ -2,7 +2,7 @@
 Modular Cubes, part 1
 Problem 271
 For a positive number n, define S(n) as the sum of the integers x, for which 1<x<n and
-x3≡1 mod n.
+x^3≡1 mod n.
 
 When n=91, there are 8 possible values for x, namely : 9, 16, 22, 29, 53, 74, 79, 81.
 Thus, S(91)=9+16+22+29+53+74+79+81=363.
