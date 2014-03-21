0f928c1ca78a92143ec215c5cff49e9ecbdb8b25
mitcc <mitcc@qq.com>
2014-03-21 17:05:15 +0800

    2014.03.21

--- project-euler/pe039.py
+++ project-euler/pe039.py
@@ -9,17 +9,19 @@ b,c}, there are exactly three solutions for p = 120.
 For which value of p ≤ 1000, is the number of solutions maximised?
 '''
 import math
+import time
 
 __date__ = '14-3-21'
 __author__ = 'SUN'
 
 if __name__ == '__main__':
+    start = time.clock()
     res = 0
     cnt = 0
     for k in range(12, 1001):
         n = 0
-        for i in range(1, int(k * (1 - math.sqrt(2) / 2))):
-            for j in range(int(k * (1 - math.sqrt(2) / 2)), k):
+        for i in range(1, int(k * 0.3)):
+            for j in range(int(k * (1 - math.sqrt(2) / 2)), k // 2):
                 if i ** 2 + j ** 2 == (k - i - j) ** 2:
                     n += 1
                     print('k = ', k, ', a = ', i, ', b = ', j, ', c = ', k - i - j)
@@ -27,3 +29,4 @@ if __name__ == '__main__':
             res = k
             cnt = n
     print('res = ', res, ', cnt = ', cnt)
+    print('Runtime is ', time.clock() - start)
