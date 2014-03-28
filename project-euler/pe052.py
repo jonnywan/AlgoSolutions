e4c7f9df6a5de2016b91b4f57878d5a138d1f442
mitcc <mitcc@qq.com>
2014-03-28 23:00:58 +0800

    2014.03.28

---
+++ project-euler/pe052.py
@@ -0,0 +1,15 @@
+"""
+Permuted multiples
+Problem 52
+It can be seen that the number, 125874, and its double, 251748, contain exactly
+the same digits, but in a different order.
+
+Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
+contain the same digits.
+"""
+__date__ = '14-3-28'
+__author__ = 'SUN'
+
+if __name__ == '__main__':
+    res = 1
+    
