53297675b8609fc702271d940d69e3cb3b7d62c2
mitcc <mitcc@qq.com>
2014-03-28 23:11:17 +0800

    2014.03.28

--- project-euler/pe052.py
+++ project-euler/pe052.py
@@ -7,9 +7,18 @@ the same digits, but in a different order.
 Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
 contain the same digits.
 """
+import time
+
 __date__ = '14-3-28'
 __author__ = 'SUN'
 
 if __name__ == '__main__':
-    res = 1
-    
+    start = time.clock()
+    x = 1
+    while True:
+        if sorted(str(x)) == sorted(str(2 * x)) == sorted(str(3 * x)) == \
+                sorted(str(4 * x)) == sorted(str(5 * x)) == sorted(str(6 * x)):
+            break
+        x += 1
+    print(x)
+    print('Runtime is ', time.clock() - start)
