e1fda921e8630d3e139480a35bc66102bd3d6dfb
mitcc <mitcc@qq.com>
2014-04-05 01:21:38 +0800

    2014.04.05

--- project-euler/pe069.py
+++ project-euler/pe069.py
@@ -25,22 +25,27 @@ import time
 __date__ = '14-4-4'
 __author__ = 'SUN'
 
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-def phi(n):
-    return [gcd(i, n) == 1 for i in range(1, n)].count(True)
+def prime_sieve(limit):
+    is_prime = [True] * (limit + 1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(limit ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i ** 2, limit + 1, i):
+                is_prime[j] = False
+    prime = []
+    for i in range(limit + 1):
+        if is_prime[i]:
+            prime.append(i)
+    return prime
 
 if __name__ == '__main__':
     start = time.clock()
     N = 1000000
-    answer = 0
-    max_value = 0
-    for i in range(2, N + 1):
-        if max_value < i / phi(i):
-            answer = i
-            max_value = i / phi(i)
+    prime = prime_sieve(30)
+    answer = 1
+    for x in prime:
+        if answer * x > N:
+            break
+        answer *= x
     print(answer)
     print('Runtime is ', time.clock() - start)
