1ff8a897101c40a2c9c8f58e3adece17211a9ba0
mitcc <mitcc@qq.com>
2014-04-09 15:07:31 +0800

    2014.04.09

--- leetcode/RestoreIPAddresses.java
+++ leetcode/RestoreIPAddresses.java
@@ -1,5 +1,6 @@
 /*
- * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ * Given a string containing only digits, restore it by returning all possible 
+ * valid IP address combinations.
 
  * For example:
  * Given "25525511135",
@@ -7,57 +8,30 @@
  * return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
  */
 public class RestoreIPAddresses {
+
+/**************************** updated 2014.04.09 *****************************/
+
     public ArrayList<String> restoreIpAddresses(String s) {
-        return helper(s, 4);
+        ArrayList<String> res = new ArrayList<String>();
+        dfs(res, "", s, 4);
+        return res;
     }
 
-    public ArrayList<String> helper(String s, int n) {
-        ArrayList<String> result = new ArrayList<String>();
-        if(s.length() <= 3 * n && s.length() >= n) {
-            if(n == 1) {
-                if(s.length() == 1 || s.charAt(0) != '0' && Long.parseLong(s) <= 255)
-                    result.add(s);
-            } else {
-                for(int i = 1; i < s.length() && i <= 3; i++) {
-                    String preStr = s.substring(0, i);
-                    if(i > 1 && s.charAt(0) == '0')
-                        continue;
-                    ArrayList<String> temp = helper(s.substring(i, s.length()), n - 1);
-                    if(Integer.parseInt(preStr) <= 255 && !temp.isEmpty()) {
-                        for(String item : temp) {
-                            result.add(preStr + "." + item);
-                        }
-                    }
+    public void dfs(ArrayList<String> res, String ip, String subStr, int n) {
+        if (n == 0 && subStr.length() == 0) {
+            res.add(ip);
+        } else if (n > 0) {
+            for (int i = 0; i < Math.min(subStr.length(), 3); i++) {
+                if (i > 0 && subStr.charAt(0) == '0')
+                    continue;
+                if (Integer.parseInt(subStr.substring(0, i + 1)) <= 255) {
+                    String temp = ip;
+                    ip += (n < 4 ? "." : "") + subStr.substring(0, i + 1);
+                    dfs(res, ip, subStr.substring(i + 1), n - 1);
+                    ip = temp;
                 }
-            }  
-        } 
-        return result;
-    }
-
-
-
-
-/*  public ArrayList<String> restoreIpAddresses(String s) {
-        ArrayList<String> result = new ArrayList<String>();
-        helper(result, 0, "", 0, s);
-        return result;
-    }
-
-    public void helper(ArrayList<String> result, int part, String cur, int index, String s) {
-        if(index == s.length() && part == 4) {
-            result.add(cur.substring(0, cur.length() - 1));
-            return ;
-        }
-        if(s.length() - index < 4 - part || s.length() - index > 3 * (4 - part))
-            return ;
-        int num = 0;
-        for(int i = index; i < index + 3 && i < s.length(); i++) {
-            num = 10 * num + s.charAt(i) - '0';
-            if(num <= 255) 
-                helper(result, part + 1, cur + Integer.toString(num) + ".", i + 1, s);
-            if(num == 0)
-                break;
+            }
         }
     }
-*/
+
 }
