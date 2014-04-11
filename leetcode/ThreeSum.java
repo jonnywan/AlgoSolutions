4840dcc95e0d23c4589521f236fbd1fbe9823529
mitcc <mitcc@qq.com>
2014-04-11 21:00:22 +0800

    2014.04.11

--- leetcode/ThreeSum.java
+++ leetcode/ThreeSum.java
@@ -1,10 +1,12 @@
 /*
- * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? 
+ * Given an array S of n integers, are there elements a, b, c in S such that 
+ * a + b + c = 0? 
  * Find all unique triplets in the array which gives the sum of zero.
 
  * Note:
 
- * Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ? b ? c)
+ * Elements in a triplet (a,b,c) must be in non-descending order. (ie, a <= b 
+ * <= c)
  * The solution set must not contain duplicate triplets.
  *   For example, given array S = {-1 0 1 2 -1 -4},
 
@@ -91,9 +93,5 @@ public class ThreeSum {
         }
         return output;
     }
-    
-	public static void main(String[] args) {
-		int[] s = {-2, 0, 1, 1, 2};
-		System.out.println(new ThreeSum().threeSum(s));
-	}
+
 }
