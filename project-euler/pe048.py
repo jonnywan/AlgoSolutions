caf1ccdf4ac1f7e66769dd2bb045cb4742339e6d
mitcc <mitcc@qq.com>
2014-04-19 16:01:29 +0800

    2014.04.19

--- project-euler/pe048.py
+++ project-euler/pe048.py
@@ -9,7 +9,7 @@ Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 __author__ = 'SUN'
 
 if __name__ == '__main__':
-    res = 0;
+    res = 0
     for i in range(1, 1001):
         res += i ** i
     print(res % (10 ** 10))
