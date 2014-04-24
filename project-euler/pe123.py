d4f7759f5f0536982d765c04b7d68ec34e8a10fd
mitcc <mitcc@qq.com>
2014-04-24 17:37:24 +0800

    2014.04.24

--- project-euler/pe123.py
+++ project-euler/pe123.py
@@ -10,10 +10,17 @@ The least value of n for which the remainder first exceeds 10^9 is 7037.
 
 Find the least value of n for which the remainder first exceeds 10^10.
 """
+import time
 from pe069 import prime_sieve
 
 __date__ = '14-4-24'
 __author__ = 'SUN'
 
 if __name__ == '__main__':
-    prime = prime_sieve(1000000)
+    start = time.clock()
+    prime = prime_sieve(300000)
+    for i in range(0, len(prime), 2):
+        if 2 * (i + 1) * prime[i] % prime[i] ** 2 > 10000000000:
+            print(i + 1)
+            break
+    print('Runtime is', time.clock() - start)
