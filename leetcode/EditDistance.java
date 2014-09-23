ef1dd7ce19d7271ab7f7120af6cb696af627b857
mitcc <mitcc@qq.com>
2014-09-23 19:07:29 +0800

    2014.09.23

--- leetcode/EditDistance.java
+++ leetcode/EditDistance.java
@@ -1,5 +1,6 @@
 /*
- * Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. 
+ * Given two words word1 and word2, find the minimum number of steps required 
+ * to convert word1 to word2. 
  * (each operation is counted as 1 step.)
 
  * You have the following 3 operations permitted on a word:
@@ -10,19 +11,24 @@
  */
 public class EditDistance {
     public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
-        for(int i = 0; i <= word1.length(); i++) 
+        int word1Len = word1.length();
+        int word2Len = word2.length();
+        int[][] dp = new int[word1Len + 1][word2Len + 1];
+        for (int i = 1; i <= word1Len; i++) {
             dp[i][0] = i;
-        for(int i = 0; i <= word2.length(); i++)
+        }
+        for (int i = 1; i <= word2Len; i++) {
             dp[0][i] = i;
-        for(int i = 1; i <= word1.length(); i++) {
-            for(int j = 1; j <= word2.length(); j++) {
-                if(word1.charAt(i - 1) == word2.charAt(j - 1))
+        }
+        for (int i = 1; i <= word1Len; i++) {
+            for (int j = 1; j <= word2Len; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1))
                     dp[i][j] = dp[i - 1][j - 1];
                 else 
-                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, 
+                            Math.min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
             }
         }
-        return dp[word1.length()][word2.length()];
+        return dp[word1Len][word2Len]; 
     }
 }
