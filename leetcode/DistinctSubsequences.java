5959c42fa21a3400632ad32362c92bbe44173d3f
mitcc <mitcc@qq.com>
2014-09-25 00:24:02 +0800

    2014.09.25

--- leetcode/DistinctSubsequences.java
+++ leetcode/DistinctSubsequences.java
@@ -36,9 +36,9 @@ public class DistinctSubsequences {
     public int numDistinct(String S, String T) {
         int[] dp = new int[T.length() + 1];
         dp[0] = 1;
-        for(int i = 0; i < S.length(); i++) {
-            for(int j = T.length() - 1; j >= 0; j--) {
-                if(S.charAt(i) == T.charAt(j))
+        for (int i = 0; i < S.length(); i++) {
+            for (int j = T.length() - 1; j >= 0; j--) {
+                if (S.charAt(i) == T.charAt(j))
                     dp[j + 1] += dp[j];
             }
         }
