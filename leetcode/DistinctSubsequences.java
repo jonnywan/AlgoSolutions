829eecd09ba7bcb809e4955d775204ad3821e380
mitcc <mitcc@qq.com>
2014-09-24 23:21:11 +0800

    2014.09.24

--- leetcode/DistinctSubsequences.java
+++ leetcode/DistinctSubsequences.java
@@ -11,19 +11,28 @@
  * Return 3. 
  */
 public class DistinctSubsequences {
-/*
+
     public int numDistinct(String S, String T) {
-        int[][] dp = new int[S.length() + 1][T.length() + 1];
-        for(int i = 0; i < S.length(); i++)
-            dp[i][0] = 1;
-        for(int i = 1; i <= S.length(); i++) {
-            for(int j = 1; j <= T.length(); j++) {
-                dp[i][j] = dp[i - 1][j] + ((S.charAt(i - 1) == T.charAt(j - 1)) ? dp[i - 1][j - 1] : 0);
+        int sLength = S.length();
+        int tLength = T.length();
+        int[][] dp = new int[tLength + 1][sLength + 1];
+        for (int i = 0; i < sLength; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i <= tLength; i++) {
+            for (int j = 1; j <= sLength; j++) {
+                dp[i][j] = dp[i][j - 1];
+                if (T.charAt(i - 1) == S.charAt(j - 1)) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                }
             }
         }
-        return dp[S.length()][T.length()];
+        return dp[tLength][sLength];
     }
-*/
+
+/**************************** updated 2014.09.24 *****************************/
+
+
     public int numDistinct(String S, String T) {
         int[] dp = new int[T.length() + 1];
         dp[0] = 1;
