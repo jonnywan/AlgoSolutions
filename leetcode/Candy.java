c52a3a1cb2f51e4347ca713afd98e98bcc663936
mitcc <mitcc@qq.com>
2014-10-12 00:32:37 +0800

    updated Candy.java

--- leetcode/Candy.java
+++ leetcode/Candy.java
@@ -13,17 +13,17 @@
 public class Candy {
 
     public int candy(int[] ratings) {
-        int[] candy = new int[ratings.length];
+        int[] candies = new int[ratings.length];
         for (int i = 0; i < ratings.length; i++) {
-            candy[i] = (i > 0 && ratings[i] > ratings[i - 1]) ? 
-                candy[i - 1] + 1 : 1;
+            candies[i] = (i > 0 && ratings[i] > ratings[i - 1]) ? 
+                candies[i - 1] + 1 : 1;
         } 
-        int result = candy[ratings.length - 1];
+        int result = candies[ratings.length - 1];
         for (int i = ratings.length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
-                candy[i] = candy[i + 1] + 1;
+            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                candies[i] = candies[i + 1] + 1;
             }
-            result += candy[i];
+            result += candies[i];
         }
         return result;
     }
