eb0f5bb6d03f4783ff030b3a37eed0ee794339f5
mitcc <mitcc@qq.com>
2014-10-12 20:21:51 +0800

    updated 20141012

--- leetcode/SingleNumber.java
+++ leetcode/SingleNumber.java
@@ -1,12 +1,15 @@
 /*
- * Given an array of integers, every element appears twice except for one. Find that single one.
+ * Given an array of integers, every element appears twice except for one. Find 
+ * that single one.
 
  * Note:
- * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ * Your algorithm should have a linear runtime complexity. Could you implement 
+ * it without using extra memory?
  * 
  */
 public class SingleNumber {
-/*  public int singleNumber(int[] A) {
+
+    public int singleNumber(int[] A) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int i = 0; i < A.length; i++) {
             map.put(A[i], map.containsKey(A[i]) ? map.get(A[i]) + 1 : 1);
@@ -17,11 +20,12 @@ public class SingleNumber {
         }
         return A[i];
     }
-*/
+
+/*****************************updated 20141012********************************/
 
     public int singleNumber(int[] A) {
-        int result = 0;
-        for(int i = 0; i < A.length; i++) {
+        int result = A[0];
+        for (int i = 1; i < A.length; i++) {
             result ^= A[i];
         }
         return result;
