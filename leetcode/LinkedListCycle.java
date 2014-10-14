632e2a5ec9199f4adec93c9dd67d160cb9274459
mitcc <mitcc@qq.com>
2014-10-14 10:47:51 +0800

    updated 20141014

--- leetcode/LinkedListCycle.java
+++ leetcode/LinkedListCycle.java
@@ -16,14 +16,17 @@
  * }
  */
 public class LinkedListCycle {
+
     public boolean hasCycle(ListNode head) {
-        ListNode cur1 = head, cur2 = head;
-        while(cur2 != null && cur2.next != null) {
+        ListNode cur1 = head;
+        ListNode cur2 = head;
+        while (cur2 != null && cur2.next != null) {
             cur1 = cur1.next;
             cur2 = cur2.next.next;
-            if(cur1 == cur2) 
+            if (cur1 == cur2)
                 return true;
         }
         return false;
     }
+
 }
