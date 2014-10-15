c6420b8a20f272e234202364323d9e89f5e757a7
mitcc <mitcc@qq.com>
2014-10-15 14:06:33 +0800

    updated 20141015

--- leetcode/LinkedListCycleII.java
+++ leetcode/LinkedListCycleII.java
@@ -1,5 +1,6 @@
 /*
- * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ * Given a linked list, return the node where the cycle begins. If there is no
+ * cycle, return null.
 
  * Follow up:
  * Can you solve it without using extra space?
@@ -16,25 +17,27 @@
  * }
  */
 public class LinkedListCycleII {
-    public ListNode findCycle(ListNode head) {
-        /* If the list has a cycle, return the node where the cycle starts with. 
-         * If there is no cycle, return null*/
-        ListNode cur1 = head, cur2 = head;
-        while(cur2 != null && cur2.next != null) {
-            cur1 = cur1.next;
+/**************************** updated 20141015 *******************************/
+
+    public ListNode findIntersection(ListNode head) {
+        ListNode cur1 = head;
+        ListNode cur2 = head;
+        while (cur2 != null && cur2.next != null) {
+            cur1 = cur1.next; 
             cur2 = cur2.next.next;
-            if(cur1 == cur2)
+            if (cur1 == cur2)
                 return cur1;
         }
         return null;
     }
 
     public ListNode detectCycle(ListNode head) {
-        ListNode cur1 = findCycle(head), cur2 = head;
-        while(cur1 != null && cur1 != cur2) {
+        ListNode cur1 = head;
+        ListNode cur2 = findIntersection(head);
+        while (cur2 != null && cur1 != cur2) {
             cur1 = cur1.next;
             cur2 = cur2.next;
         }
-        return cur1;
+        return cur2;
     }
 }
