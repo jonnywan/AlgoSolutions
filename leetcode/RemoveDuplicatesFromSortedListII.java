c895dfd07c0da527783ef3da5d827797c5b1db32
mitcc <mitcc@qq.com>
2014-10-19 12:26:55 +0800

    updated 20141019

--- leetcode/RemoveDuplicatesFromSortedListII.java
+++ leetcode/RemoveDuplicatesFromSortedListII.java
@@ -51,4 +51,25 @@ public class RemoveDuplicatesFromSortedListII {
             return deleteDuplicates(cur2);
         }
     }
+
+/**************************** updated 20141019 *******************************/
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode cur = prev.next;
+        while (cur != null) {
+            if (cur.next != null && cur.val == cur.next.val) {
+                cur = cur.next;
+            } else {
+                if (prev.next == cur)
+                    prev = prev.next;
+                else
+                    prev.next = cur.next;
+                cur = prev.next;
+            }
+        }
+        return dummy.next;
+    }
 }
