50aea4c80988779918fb48152e5a44c3fa2c23d9
mitcc <mitcc@qq.com>
2014-10-21 22:20:11 +0800

    updated 20141021

--- leetcode/PascalTriangleII.java
+++ leetcode/PascalTriangleII.java
@@ -8,21 +8,21 @@
  * Could you optimize your algorithm to use only O(k) extra space? 
  */
 public class PascalTriangleII {
-/**************************** updated 2013/11/26 *********************/
-    public ArrayList<Integer> getRow(int rowIndex) {
-        ArrayList<Integer> res = new ArrayList<Integer>(), temp = new ArrayList<Integer>();
-        res.add(1);
-        temp.add(1);
-        temp.add(1);
-        for(int i = 1; i <= rowIndex; i++) {
-            res = new ArrayList<Integer>();
-            res.add(1);
-            for(int j = 0; j <= i - 2; j++) 
-                res.add(temp.get(j) + temp.get(j + 1));
-            res.add(1);
-            temp = res;
+
+/**************************** updated 20141021 *******************************/
+
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<Integer>();
+        result.add(1);
+        for (int i = 1; i <= rowIndex; i++) {
+            List<Integer> temp = new ArrayList<Integer>();
+            temp.add(1);
+            for (int j = 1; j < result.size(); j++) 
+                temp.add(result.get(j - 1) + result.get(j));
+            temp.add(1);
+            result = temp;
         }
-        return res;
+        return result;
     }
 
 /*********************************************************************/
@@ -30,9 +30,9 @@ public class PascalTriangleII {
     public ArrayList<Integer> getRow(int rowIndex) {
         ArrayList<Integer> res = new ArrayList<Integer>();
         res.add(1);
-        if(rowIndex > 0) {
+        if (rowIndex > 0) {
             ArrayList<Integer> temp = getRow(rowIndex - 1);
-            for(int i = 0; i <= rowIndex - 2; i++) 
+            for (int i = 0; i <= rowIndex - 2; i++) 
                 res.add(temp.get(i) + temp.get(i + 1));
             res.add(1);
         }
