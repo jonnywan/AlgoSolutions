2e2e8892f10c6511b18443f14ca1b86dd16d6f34
mitcc <mitcc@qq.com>
2014-10-22 23:41:51 +0800

    updated 20141022

--- leetcode/PascalTriangle.java
+++ leetcode/PascalTriangle.java
@@ -14,22 +14,23 @@
  */
 public class PascalTriangle {
 
-    public ArrayList<ArrayList<Integer>> generate(int numRows) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        if(numRows <= 0)
-            return res;
-        ArrayList<Integer> first = new ArrayList<Integer>();
-        first.add(1);
-        res.add(first);
-        for(int i = 2; i <= numRows; i++) {
-            ArrayList<Integer> prev = res.get(i - 2), next = new ArrayList<Integer>();
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (numRows == 0)
+            return result;
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        result.add(list);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> next = new ArrayList<Integer>();
             next.add(1);
-            for(int j = 1; j < i - 1; j++) 
-                next.add(prev.get(j - 1) + prev.get(j));
+            for (int j = 1; j < list.size(); j++) 
+                next.add(list.get(j - 1) + list.get(j));
             next.add(1);
-            res.add(next);
+            list = next;
+            result.add(list);
         }
-        return res;
+        return result;
     }
 
 }
