79af7b7192e987b2a770f6091c4ace3d5238b91d
mitcc <mitcc@qq.com>
2014-10-25 20:27:06 +0800

    updated 20141025

--- leetcode/MaximumSubarray.java
+++ leetcode/MaximumSubarray.java
@@ -1,23 +1,26 @@
-/* Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+/* Find the contiguous subarray within an array (containing at least one 
+ * number) which has the largest sum.
 
  * For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
  * the contiguous subarray [4,−1,2,1] has the largest sum = 6.
  */ 
 public class MaximumSubarray {
-/********************** updated 2013/11/26 *********************/
+
     public int maxSubArray(int[] A) {
-        int max = Integer.MIN_VALUE, tempMax = 0;
-        for(int i = 0; i < A.length; i++) {
-            tempMax += A[i];
-            if(max < tempMax) 
-                max = tempMax;
-            if(tempMax < 0)
-                tempMax = 0;
+        int result = Integer.MIN_VALUE;
+        int thisSum = 0;
+        for (int i = 0; i < A.length; i++) {
+            thisSum += A[i];
+            if (result < thisSum)
+                result = thisSum;
+            if (thisSum < 0)
+                thisSum = 0;
         }
-        return max;
+        return result;
     }
 
 /****************** Below is from wikipedia ******************/
+
     public int maxSubArray(int[] A) {
         int max_so_far = A[0], max_ending_here = A[0];
         for(int i = 1; i < A.length; i++) {//recursion starts from 1.
