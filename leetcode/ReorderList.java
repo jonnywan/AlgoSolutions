8ccacbcc9b487bf86945cf2a165c6672d4f17979
mitcc <mitcc@qq.com>
2014-10-28 01:06:55 +0800

    :bug::bug::bug::bug::bug:

--- leetcode/ReorderList.java
+++ leetcode/ReorderList.java
@@ -10,6 +10,7 @@
  * }
  */
 public class ReorderList {
+    //Time Limit Exceeded.
     public void reorderList(ListNode head) {
         if(head == null || head.next == null || head.next.next == null)
             return;
@@ -25,4 +26,54 @@ public class ReorderList {
         head = head.next;
         head.next = subList;
     }
+
+/**************************** added 20141028 *********************************/
+    // Cannot AC, some bugs to be fixed.
+
+    public void reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+        ListNode prev = head;
+        ListNode cur = prev.next;
+        while (cur != null) {
+            prev.next = cur.next;
+            cur.next = head;
+            head = cur;
+            cur = prev.next;
+        }
+    }
+
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null)
+            return;
+        int len = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            len++;
+            cur = cur.next;
+        }
+        ListNode lastHalf = null;
+        cur = head;
+        for (int i = 0; i < len / 2; i++) {
+            cur = cur.next;
+        }
+        lastHalf = cur.next;
+        cur.next = null;
+        reverseList(lastHalf);
+        cur = head;
+        ListNode cur1 = head.next;
+        ListNode cur2 = lastHalf;
+        for (int i = 1; i < len; i++) {
+            if (i % 2 == 1) {
+                cur.next = cur2;
+                cur2 = cur2.next;
+                cur = cur.next;
+            } else {
+                cur.next = cur1;
+                cur1 = cur1.next;
+                cur = cur.next;
+            }
+        }
+    }
+
 }
