e8e321bbf54ccb350e33f10ff47989dd11e5c487
mitcc <mitcc@qq.com>
2014-10-30 00:17:25 +0800

    :bug::bug:

--- leetcode/ReorderList.java
+++ leetcode/ReorderList.java
@@ -19,22 +19,6 @@
  * }
  */
 public class ReorderList {
-    //Time Limit Exceeded.
-    public void reorderList(ListNode head) {
-        if(head == null || head.next == null || head.next.next == null)
-            return;
-        ListNode subList = head.next;
-        ListNode cur1 = head, cur2 = head.next;
-        while(cur2.next != null) {
-            cur1 = cur1.next;
-            cur2 = cur2.next;
-        }
-        head.next = cur2;
-        cur1.next = null;
-        reorderList(subList);
-        head = head.next;
-        head.next = subList;
-    }
 
 /**************************** added 20141028 *********************************/
     // Cannot AC, some bugs to be fixed.
