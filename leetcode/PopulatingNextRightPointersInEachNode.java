b05b8a8344543e95d9b8fb94bbb64ac5f80335cb
mitcc <mitcc@qq.com>
2014-11-02 20:13:34 +0800

    :cat:

--- leetcode/PopulatingNextRightPointersInEachNode.java
+++ leetcode/PopulatingNextRightPointersInEachNode.java
@@ -74,24 +74,6 @@ public class PopulatingNextRightPointersInEachNode {
     }
 */
 
-
-    public void connect(TreeLinkNode root) {
-        if(root == null)
-            return;
-        while(root != null) {
-            TreeLinkNode cur = root;
-            while(cur != null) {
-                if(cur.left != null)
-                    cur.left.next = cur.right;
-                if(cur.right != null && cur.next != null)
-                    cur.right.next = cur.next.left;
-                cur = cur.next;
-            }
-            root = root.left;
-        }
-    }
-
-
 /***************************** updated 20141102 ******************************/
 
     public void connect(TreeNode root) {
