51afd5684db6d8dd83b4f0bae598ec64250cf41c
mitcc <mitcc@qq.com>
2014-11-09 01:44:30 +0800

    :octocat:

--- leetcode/SortList.java
+++ leetcode/SortList.java
@@ -13,28 +13,6 @@
  * }
  */
 public class SortList {
-    // Runtime Error, to be debugged!!! 
-    public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null)
-            return head;
-        ListNode minNode = head, minPrev = head, minNext = minNode.next;
-        ListNode cur = head.next, curPrev = head;
-        while(cur != null) {
-            if(minNode.val > cur.val) {
-                minNode = cur;
-                minPrev = curPrev;
-                minNext = cur.next;
-            }
-            curPrev = cur;
-            cur = cur.next;
-        }
-        if(minNode == head)
-            head = head.next;
-        else 
-            minPrev.next = minNext;
-        minNode.next = sortList(head);
-        return minNode;
-    }
 
 /**************************** cheating method *******************************/
 
