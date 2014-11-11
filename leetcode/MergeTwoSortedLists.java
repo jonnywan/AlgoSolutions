89dd7a633f18579fe785599a7e01bcaed9586c38
mitcc <mitcc@qq.com>
2014-11-11 23:32:12 +0800

    :octocat:

--- leetcode/MergeTwoSortedLists.java
+++ leetcode/MergeTwoSortedLists.java
@@ -14,19 +14,20 @@
  * }
  */
 public class MergeTwoSortedLists {
+
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null)
+        if (l1 == null)
             return l2;
-        else if(l2 == null)
+        if (l2 == null)
             return l1;
-        else if(l1.val <= l2.val) {
+        if (l1.val < l2.val) {
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
         } else {
             l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
-    }
+    } 
 	
 /*************************************************************/
 
