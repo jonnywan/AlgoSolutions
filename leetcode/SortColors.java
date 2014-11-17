43eb02c2b34ded35c73f599bd5b35d33a4e537ba
mitcc <mitcc@qq.com>
2014-11-17 22:08:33 +0800

    :octocat:

--- leetcode/SortColors.java
+++ leetcode/SortColors.java
@@ -3,20 +3,40 @@
  * objects of the same color are adjacent, with the colors in the order red, 
  * white and blue.
 
- * Here, we will use the integers 0, 1, and 2 to represent the color red, white, 
- * and blue respectively.
+ * Here, we will use the integers 0, 1, and 2 to represent the color red, 
+ * white, and blue respectively.
 
  * Note:
  * You are not suppose to use the library's sort function for this problem.
 
  * Follow up:
- * A rather straight forward solution is a two-pass algorithm using counting sort.
- * First, iterate the array counting number of 0's, 1's, and 2's, then overwrite 
- * array with total number of 0's, then 1's and followed by 2's.
+ * A rather straight forward solution is a two-pass algorithm using counting 
+ * sort.
+ * First, iterate the array counting number of 0's, 1's, and 2's, then 
+ * overwrite array with total number of 0's, then 1's and followed by 2's.
 
- * Could you come up with an one-pass algorithm using only constant space? 
+ * Could you come up with an one-pass algorithm using only constant space?
+ * 
  */
 public class SortColors {
+
+    public void sortColors(int[] A) {
+        int left = 0;
+        int right = A.length - 1;
+        for (int i = 0; i <= right; i++) {
+            if (A[i] == 0) 
+                swap(A, i, left++);
+            else if (A[i] == 2)
+                swap(A, i--, right--);
+        }
+    }
+
+    public void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+    
 /**************************** updated 2013/12/11 *****************************/
 
     public void sortColors(int[] A) {
@@ -31,38 +51,22 @@ public class SortColors {
 /*****************************************************************************/
 
     public void sortColors(int[] A) {
-        int redCount = 0, whiteCount = 0, blueCount = 0;
-        for(int i = 0; i < A.length; i++) {
-            if(A[i] == 0)
-                redCount++;
-            else if(A[i] == 1)
-                whiteCount++;
-            else
-                blueCount++;
+        int zeroes = 0;
+        int ones = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0)
+                zeroes++;
+            else if (A[i] == 1)
+                ones++;
         }
-        for(int i = 0; i < A.length; i++) {
-            if(i < redCount)
+        for (int i = 0; i < A.length; i++) {
+            if (i < zeroes) 
                 A[i] = 0;
-            else
-                A[i] = i < redCount + whiteCount ? 1 : 2;
+            else if (i < zeroes + ones)
+                A[i] = 1;
+            else 
+                A[i] = 2;
         }
     }
-
-/*****************************************************************************/
-
-    public void sortColors(int[] A) {
-        int left = 0, right = A.length - 1;
-        for(int i = 0; i <= right; i++) {
-            if(A[i] == 0)
-                swap(A, i, left++);
-            else if(A[i] == 2) 
-                swap(A, i--, right--);
-        }
-    }
-
-    public void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+    
 }
