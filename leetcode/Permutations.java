1be01261bb2741c9aae210083fb729d29a7cc345
mitcc <mitcc@qq.com>
2014-11-20 15:26:24 +0800

    :octocat:

--- leetcode/Permutations.java
+++ leetcode/Permutations.java
@@ -5,28 +5,24 @@
  * [1,2,3] have the following permutations:
  * [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
  */
-import java.util.*;
 
 public class Permutations {
-/*************************** updated 2013/11/29 ******************************/
 
-    public ArrayList<ArrayList<Integer>> permute(int[] num) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        if(num.length == 0)
-            return res;
-        res.add(new ArrayList<Integer>());
-        for(int i = 0; i < num.length; i++) {
-            ArrayList<ArrayList<Integer>> newRes = new ArrayList<ArrayList<Integer>>();
-            for(int j = 0; j < res.size(); j++) {
-                for(int k = 0; k <= res.get(j).size(); k++) {
-                    ArrayList<Integer> list = new ArrayList<Integer>(res.get(j));
-                    list.add(k, num[i]);
-                    newRes.add(list);
+    public List<List<Integer>> permute(int[] num) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        result.add(new ArrayList<Integer>());
+        for (int i = 0; i < num.length; i++) {
+            List<List<Integer>> tmp = new ArrayList<List<Integer>>();
+            for (List<Integer> list : result) {
+                for (int j = 0; j <= list.size(); j++) {
+                    List<Integer> item = new ArrayList<Integer>(list);
+                    item.add(j, num[i]);
+                    tmp.add(item);
                 }
             }
-            res = newRes;
+            result = tmp;
         }
-        return res;
+        return result;
     }
 
 /**************************** updated 2014/01/05 *****************************/
