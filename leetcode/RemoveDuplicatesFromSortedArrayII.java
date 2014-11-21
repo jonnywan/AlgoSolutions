ac1055c0cec016b4f260d15e26437c04312d8b3d
mitcc <mitcc@qq.com>
2014-11-22 02:42:47 +0800

    :octocat:

--- leetcode/RemoveDuplicatesFromSortedArrayII.java
+++ leetcode/RemoveDuplicatesFromSortedArrayII.java
@@ -10,29 +10,32 @@
 public class RemoveDuplicatesFromSortedArrayII {
 
     public int removeDuplicates(int[] A) {
-        if(A.length == 0)
-            return 0;
-        int index = 1, cnt = 1;
-        for(int i = 1; i < A.length; i++) {
-            cnt = (A[i] == A[i - 1]) ? cnt + 1 : 1;
-            if(cnt <= 2)
+        if (A.length == 0)
+            return A.length;
+        int count = 1;
+        int index = 1;
+        for (int i = 1; i < A.length; i++) {
+            count = A[i] == A[i - 1] ? count + 1 : 1;
+            if (count <= 2)
                 A[index++] = A[i];
         }
         return index;
     }
 
-/**************************** updated 2013/12/26 *****************************/
+/*****************************************************************************/
 
     public int removeDuplicates(int[] A) {
-        if(A.length == 0 || A.length == 1)
+        if (A.length == 0 || A.length == 1)
             return A.length;
-        int cnt = 2, temp = A[0], prev;
-        for(int i = 2; i < A.length; i++) {
+        int index = 2;
+        int prev = A[1];
+        int tmp = A[0];
+        for (int i = 2; i < A.length; i++) {
             prev = A[i - 1];
-            if(A[i] != A[i - 1] || A[i - 1] != temp)
-                A[cnt++] = A[i];
-            temp = prev;
+            if (A[i] != A[i - 1] || A[i] != tmp) 
+                A[index++] = A[i];
+            tmp = prev;
         }
-        return cnt;
-    }
+        return index;
+    } 
 }
