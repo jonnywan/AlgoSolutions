94dcbb0bd8c8f8c7dcc88165d97cbdfa1aade08e
mitcc <mitcc@qq.com>
2014-11-23 01:43:58 +0800

    :octocat:

--- leetcode/ContainerWithMostWater.java
+++ leetcode/ContainerWithMostWater.java
@@ -1,21 +1,27 @@
 /*
- * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). 
- * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two 
- * lines, which together with x-axis forms * a container, such that the container contains the most water.
+ * Given n non-negative integers a1, a2, ..., an, where each represents a point 
+ * at coordinate (i, ai). n vertical lines are drawn such that the two 
+ * endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together 
+ * with x-axis forms a container, such that the container contains the most 
+ * water.
 
  * Note: You may not slant the container.
  */
 public class ContainerWithMostWater {
+    
     public int maxArea(int[] height) {
-        int maxArea = 0;
-        int low = 0, high = height.length - 1;
-        while(low < high) {
-            maxArea = Math.max(maxArea, (high - low) * Math.min(height[low], height[high]));
-            if(height[low] < height[high])
-                low++;
-            else 
-                high--;
+        int result = 0;
+        int start = 0;
+        int end = height.length - 1;
+        while (start < end) {
+            result = Math.max(Math.min(height[start], height[end]) 
+                    * (end - start), result);
+            if (height[start] < height[end])
+                start++;
+            else
+                end--;
         }
-        return maxArea;
+        return result;
     }
+
 }
