fc0604af79e522703720b57a41eedb2dce644ca8
mitcc <mitcc@qq.com>
2014-11-25 20:14:15 +0800

    :octocat:

--- leetcode/GenerateParentheses.java
+++ leetcode/GenerateParentheses.java
@@ -7,7 +7,6 @@
  * "((()))", "(()())", "(())()", "()(())", "()()()" 
  */
 public class GenerateParentheses {
-/************************** updated 2013/12/10 *******************************/
 
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<String>();
@@ -26,18 +25,21 @@ public class GenerateParentheses {
 
 /*****************************************************************************/
 
-    public ArrayList<String> generateParenthesis(int n) {
-        ArrayList<String> res = new ArrayList<String>();
-        if(n == 0) 
-            res.add("");
-        else if(n == 1) 
-            res.add("()");
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<String>();
+        if (n == 0)
+            result.add("");
+        else if (n == 1)
+            result.add("()");
         else {
-            for(int i = 0; i < n; i++) 
-                for(String inner : generateParenthesis(i)) 
-                    for(String outer : generateParenthesis(n - 1 - i))
-                        res.add("(" + inner + ")" + outer);
+            for (int i = 0; i < n; i++) {
+                for (String inner : generateParenthesis(i)) {
+                    for (String outer : generateParenthesis(n - 1 - i)) {
+                        result.add("(" + inner + ")" + outer);
+                    }
+                }
+            }
         }
-        return res;
+        return result;
     }
 }
